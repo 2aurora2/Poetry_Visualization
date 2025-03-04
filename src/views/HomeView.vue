b684f12cf52f855660e0293f1b3c5edeab53d21a
xx-76 <15813157615@163.com>
2025-03-04 12:54:48 +0800

    feat：HomeView.vue添加柱状图

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,6 @@
 <template>
   <div class="layout">
-    <div class="overview-section">
-      <div class="card">
-        <h2 class="card-title">诗词数量</h2>
-        <b class="card-content">
-          唐诗：57,347首，宋词：19,484首，元曲：8771首
-        </b>
-      </div>
-      <div class="card">
-        <h2 class="card-title">诗人数量</h2>
-        <b class="card-content">
-          唐代：3662人，宋代：1489人，元代：230人
-        </b>
-      </div>
-    </div>
     <div class="content-section">
-      <ChinaMapComp class="map-comp" />
       <DynastyComparisonChart class="chart-comp" />
     </div>
   </div>
